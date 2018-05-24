12513163e41a3d84c5269851738fab3458653140
ucrDavid <dsilv022@ucr.edu>
2018-05-24 10:55:30 -0700

    added sandbox to test lights on btn press

---
+++ sandbox/sandbox/main.c
@@ -0,0 +1,96 @@
+/*
+ * sandbox.c
+ *
+ * Created: 5/24/2018 10:27:23 AM
+ * Author : David
+ */ 
+
+
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>  //include libm
+#include <avr/interrupt.h>
+#include <avr/pgmspace.h>
+#include <util/delay.h>
+#include <avr/io.h>
+
+#define usart_BAUD_RATE 9600
+#include <ucr/usart.h>
+
+#include <wiinunchuck/wiinunchuck.h>
+#include <wiinunchuck/wiinunchuck.c>
+
+enum BTN_States{BTN_start, BTN_wait, BTN_Z, BTN_C} BTN_state;
+	
+void tickFct() {
+	switch (BTN_state) {
+		case BTN_start:
+			BTN_state = BTN_wait;
+			break;
+		case BTN_wait:
+			if (wiinunchuck_getbuttonC()) {
+				BTN_state = BTN_C;
+			} else if (wiinunchuck_getbuttonZ()) {
+				BTN_state = BTN_Z;
+			}
+			break;
+		case BTN_Z:
+			if (wiinunchuck_getbuttonZ()) {
+				BTN_state = BTN_Z;
+			} else {
+				BTN_state = BTN_wait;
+			}
+			break;
+		case BTN_C:
+			if (wiinunchuck_getbuttonC()) {
+				BTN_state = BTN_C;
+			} else {
+				BTN_state = BTN_wait;
+			}
+			break;
+		default:
+			BTN_state = BTN_wait;
+	}
+	switch (BTN_state) {
+		case BTN_wait:
+			//do nothing
+			break;
+		case BTN_Z:
+			PORTA = 0x01;
+			break;
+		case BTN_C:
+			PORTA = 0x02;
+			break;
+	}
+}
+
+int main(void)
+{
+	DDRA = 0x00; PORTA = 0xFF;
+	initUSART();
+	wiinunchuck_init();
+	sei();
+	uint8_t buttonZ;
+	uint8_t buttonC;
+	int joyX;
+	int joyY;
+	int angleX;
+	int angleY;
+	int angleZ;
+	wiinunchuck_update();
+    /* Replace with your application code */
+    while (1) 
+    {
+		//buttonZ = wiinunchuck_getbuttonZ();
+		//buttonC = wiinunchuck_getbuttonC();
+		//joyX = wiinunchuck_getjoyX();
+		//joyY = wiinunchuck_getjoyY();
+		//angleX = wiinunchuck_getangleX();
+		//angleY = wiinunchuck_getangleY();
+		//angleZ = wiinunchuck_getangleZ();
+		wiinunchuck_update();
+		tickFct();
+
+    }
+}
+
